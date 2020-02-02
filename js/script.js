5ea8e25f051c4da326ccbaaa70e1b8071ca144b5
MrSallivan <nik.sallivan@gmail.com>
2020-02-02 22:52:01 +1000

    qa all

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $('.header__toggle').click(function (event) {
         $('.header__toggle, .header__menu, .body').toggleClass('active');
     });
+    $('.qa__one').click(function (event) {
+        $('.qa__1').toggleClass('active');
+    });
+    $('.qa__two').click(function (event) {
+        $('.qa__2').toggleClass('active2');
+    });
+    $('.qa__three').click(function (event) {
+        $('.qa__3').toggleClass('active3');
+    });
 });
